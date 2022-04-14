f426fac6915a47cccb0bc8be3b0773ac1dca95c3
BartlomiejOlber <300237@pw.edu.pl>
2022-04-14 16:44:48 +0200

    load data

---
+++ download_datasets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://sci2s.ugr.es/keel/dataset/data/classification/ring.zip
+unzip ring.zip
+rm ring.zip
+
+wget https://sci2s.ugr.es/keel/dataset/data/classification/winequality-white.zip
+unzip winequality-white.zip
+rm winequality-white.zip
+
+wget https://sci2s.ugr.es/keel/dataset/data/classification/satimage.zip
+unzip satimage.zip
+rm satimage.zip
---
+++ main.R
@@ -0,0 +1,4 @@
+# use one
+data <- read.table("ring.dat", header=FALSE, skip=25)
+# data <- read.table("winequality-white.dat", header=FALSE, skip=16, sep=',')
+# data <- read.table("satimage.dat", header=FALSE, skip=41)
