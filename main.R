d76d93a3aaf929440cbb35c2f6afa146634878d7
BartlomiejOlber <300237@pw.edu.pl>
2022-04-22 16:16:03 +0200

    seems to be working

--- main.R
+++ main.R
@@ -4,11 +4,27 @@ if (!require(e1071)) install.packages('e1071')
 library(e1071)
 if (!require(mltools)) install.packages('mltools')
 library(mltools)
+source("utils.R")
+#algorithm
 seed = 1
 split_ratios = c(0.75, 0.25)
-split_train_ratios = c(0.075, 0.9, 0.025)
+split_train_ratios = c(0.01, 0.98, 0.01)
+criterion_types <- list("stabilization", "certainty_growth", "certainty_threshold")
+c_type = 3
+# criterion_args <- list("similarity_threshold" = 0.97) # stabilization
+# criterion_args <- list("highest_certainty_sofar" = 0.0) # certainty_growth
+criterion_args <- list("mean_certainty_threshold" = 0.95) # certainty_threshold
+curr_patience <- 5
+patience <- 5
+sample_size = 200
+autolabel_percent = 0.05
+auroc_results <-NULL
+fmeasure_results <-NULL
+prev_pred <-NULL
+n_incorrectly_labelled <- 0
 set.seed(seed)
-# use one
+
+# load data
 # data <- read.table("ring.dat", header=FALSE, skip=25, sep = ',')
 # data <- read.table("magic.dat", header=FALSE, skip=16, sep=',')
 data <- read.table("spambase.dat", header=FALSE, skip=63, sep = ',')
@@ -18,14 +34,6 @@ n_cols = ncol(data)
 # z-score normalization
 data[1:(n_cols-1)] <- scale(data[1:(n_cols-1)])
 
-# get n_classes
-# if (is.factor(data[,n_cols])){
-#   n_classes = nlevels(data[,n_cols])  
-#   data[,n_cols] <- as.numeric(data[,n_cols])
-# }else{
-#   data[,n_cols] <- data[,n_cols] - min(data[,n_cols])
-#   n_classes = max(data[,n_cols]) + 1
-# }
 if (is.factor(data[,n_cols])){
   data[,n_cols] <- as.numeric(data[,n_cols])
 }
@@ -49,32 +57,6 @@ unlabelled_set <- shuffled_data[row.names(data) %in% (test_n + train_labelled_n 
 decision_set <- shuffled_data[row.names(data) %in%
                        (test_n + train_labelled_n + train_unlabelled_n + 1):n_rows, ]
 
-# train xgboost
-dtrain <- xgb.DMatrix(data = as.matrix(labelled_set[,1:(n_cols-1)]),
-                      label = as.numeric(labelled_set[,n_cols]))
-classifier <- xgboost(data = dtrain, max.depth = 20, eta = 0.8, nthread = 2, nrounds = 20, objective = "binary:logistic", verbose = 0)
-# train svm
-# classifier = svm(x=as.matrix(labelled_set[,1:(n_cols-1)]), y=as.numeric(labelled_set[,n_cols]), kernel = "radial", cost = 1, scale = FALSE, probability= TRUE)
-
-pred <- predict(classifier, as.matrix(test_set[1:(n_cols-1)]))
-
-auroc <- auc_roc(preds = pred, actuals = test_set[,n_cols])
-
-compute_fmeasure <- function(pred, actual) {
-  pred_labels = pred > 0.5
-  precision <- sum(pred_labels & actual) / sum(pred_labels)
-  recall <- sum(pred_labels & actual) / sum(actual)
-  fmeasure <- 2 * precision * recall / (precision + recall)
-  return (fmeasure)
-}
-
-fmeasure = compute_fmeasure(pred, test_set[,n_cols])
-
-sprintf("auroc: %f", auroc)
-sprintf("fmeasure: %f", fmeasure)
-
-
-
 # autolabel algorithm
 
 # train
@@ -85,88 +67,89 @@ sprintf("fmeasure: %f", fmeasure)
 # choose N the most certain 
 # label them
 # extend labelled, shrink unlabelled (only chosen or all considered)
-criterion_types <- list("stabilization", "certainty_growth", "certainty_threshold")
-criterion_args <- list("similarity_threshold" = 0.9) # stabilization
-# criterion_args <- list("highest_certainty_sofar" = 0.0) # certainty_growth
-# criterion_args <- list("mean_certainty_threshold" = 0.9) # certainty_threshold
-curr_patience <- 5
-patience <- 5
-sample_size = 100
-autolabel_percent = 0.1
 
-pred <- predict(classifier, as.matrix(decision_set[1:(n_cols-1)]))
-if(!is.null(prev_pred) || criterion_type == "certainty_level"){
-  stop = stop_criterion(prev_pred, pred, curr_patience, criterion_type,
-                                 criterion_args)
-  if(stop){
-    curr_patience <- curr_patience - 1
+loop_counter <- 0
+while(loop_counter < 40){
+  loop_counter = loop_counter + 1
+  cat(sprintf("iteration no. %d \n", loop_counter))
+  # train xgboost
+  dtrain <- xgb.DMatrix(data = as.matrix(labelled_set[,1:(n_cols-1)]),
+                        label = as.numeric(labelled_set[,n_cols]))
+  classifier <- xgboost(data = dtrain, max.depth = 20, eta = 0.8, nthread = 2, nrounds = 20, objective = "binary:logistic", verbose = 0)
+  # train svm
+  # classifier = svm(x=as.matrix(labelled_set[,1:(n_cols-1)]), y=as.numeric(labelled_set[,n_cols]), kernel = "radial", cost = 1, scale = FALSE, probability= TRUE)
+  
+  pred <- predict(classifier, as.matrix(test_set[1:(n_cols-1)]))
+  
+  auroc <- auc_roc(preds = pred, actuals = test_set[,n_cols])
+  
+  fmeasure <- compute_fmeasure(pred, test_set[,n_cols])
+  
+  cat(sprintf("auroc: %f \n", auroc))
+  cat(sprintf("fmeasure: %f \n", fmeasure))
+  if(!is.null(auroc_results)){
+    auroc_results <- c(auroc_results, auroc)  
   }else{
-    curr_patience <- patience
+    auroc_results <- auroc
   }
-  if(curr_patience == 0)
-    break
-}
-
-stop_stabilization <- function(prev_pred, pred){
-  pred_labels = round(pred)
-  prev_pred_labels = round(prev_pred)
-  similarity = sum(!(pred_labels ^ prev_pred_labels)) / length(pred_labels)
-  return(similarity >= criterion_args$similarity_threshold)
-}
-
-stop_certainty_growth <- function(pred){
-  mean_certainty = abs(1 - mean(abs(pred - round(pred))))
-  if(mean_certainty < criterion_args$highest_certainty_sofar){
-    return(TRUE)
+  
+  if(!is.null(fmeasure_results)){
+    fmeasure_results <- c(fmeasure_results, fmeasure)
   }else{
-    criterion_args$highest_certainty_sofar <- mean_certainty
-    return(FALSE)
+    fmeasure_results <- fmeasure
   }
+  
+  
+  pred <- predict(classifier, as.matrix(decision_set[1:(n_cols-1)]))
+  
+  # auroc <- auc_roc(preds = pred, actuals = decision_set[,n_cols])
+  # 
+  # fmeasure <- compute_fmeasure(pred, decision_set[,n_cols])
+  # 
+  # cat(sprintf("auroc: %f \n", auroc))
+  # cat(sprintf("fmeasure: %f \n", fmeasure))
+  
+  if(!is.null(prev_pred) || criterion_types[c_type] == "certainty_threshold"){
+    stop <- stop_criterion(prev_pred, pred)
+    if(stop){
+      curr_patience <- curr_patience - 1
+    }else{
+      curr_patience <- patience
+    }
+    if(curr_patience == 0)
+      break
+  }
+  
+  prev_pred <- pred
+  
+  sample_ids <- sample(nrow(unlabelled_set), sample_size)
+  sample_rows <- unlabelled_set[sample_ids,]
+  
+  
+  pred <- predict(classifier, as.matrix(sample_rows[1:(n_cols-1)]))
+  
+  # auroc <- auc_roc(preds = pred, actuals = sample_rows[,n_cols])
+  # 
+  # fmeasure <- compute_fmeasure(pred, sample_rows[,n_cols])
+  # 
+  # cat(sprintf("auroc: %f \n", auroc))
+  # cat(sprintf("fmeasure: %f \n", fmeasure))
+  
+  
+  most_certain <- choose_most_certain(sample_rows, pred)
+  
+  unlabelled_set <- unlabelled_set[!(row.names(unlabelled_set) %in% row.names(most_certain)),]
+
+  labelled_set <- rbind(labelled_set, most_certain)
+  
+  print(nrow(labelled_set))
+
+  # curiosity check
+  n_incorrectly_labelled = n_incorrectly_labelled + sum(sample_rows[row.names(sample_rows) %in% row.names(most_certain), n_cols] != most_certain[,n_cols])  
+  n_labelled = nrow(labelled_set) - train_labelled_n
+  cat(sprintf("number of incorrectly labelled: %d out of %d labelled \n", n_incorrectly_labelled, n_labelled))
+  
 }
-
-stop_certainty_level <- function(pred){
-  mean_uncertainty = mean(abs(pred - round(pred)))
-  return(abs(1 - mean_uncertainty) < criterion_args$mean_certainty_threshold)
-}
-
-stop_criterion <- function(prev_pred, pred){
-  return(
-    switch(
-      criterion_type,
-      stabilization={stop_stabilization(prev_pred, pred)},
-      certainty_growth={stop_certainty_growth(pred)},
-      certainty_level={stop_certainty_level(pred)}
-    )
-  )
-}
-
-prev_pred <- pred
-
-sample_ids <- sample(nrows(unlabelled_set), sample_size)
-sample_rows <- unlabelled_set[sample_ids,]
-pred <- predict(classifier, as.matrix(sample_rows[1:(n_cols-1)]))
-
-choose_most_certain <- function(sample_rows, pred){
-  percentile = quantile(pred, 1 - autolabel_percent)
-  most_certain = sample_rows[pred > percentile,]
-  most_certain[, n_cols] = round(pred[pred > percentile])
-  return(most_certain)
-}
-
-# remove_labelled <- function(sample_rows, pred){
-#   percentile = quantile(pred, 1 - autolabel_percent)
-#   most_certain = sample_rows[pred > percentile,]
-#   most_certain[, n_cols] = round(pred[pred > percentile])
-#   unlabelled_set <- unlabelled_set[row.names(unlabelled_set) %in% row.names(most_certain)]
-#   labelled_set <- rbind(labelled_set, most_certain)
-# }
-
-most_certain <- choose_most_certain(sample_rows, pred)
-unlabelled_set <- unlabelled_set[row.names(unlabelled_set) %in% row.names(most_certain)]
-labelled_set <- rbind(labelled_set, most_certain)
-
-
-
-
-
-
+# do something with the results
+print(auroc_results)
+print(fmeasure_results)
--- utils.R
+++ utils.R
@@ -1,40 +1,50 @@
 stop_stabilization <- function(prev_pred, pred){
   pred_labels = round(pred)
   prev_pred_labels = round(prev_pred)
-  similarity = sum(!(pred_labels ^ prev_pred_labels)) / length(pred_labels)
+  similarity = sum(!xor(pred_labels, prev_pred_labels)) / length(pred_labels)
   return(similarity >= criterion_args$similarity_threshold)
 }
 
 stop_certainty_growth <- function(pred){
+
   mean_certainty = abs(1 - mean(abs(pred - round(pred))))
   if(mean_certainty < criterion_args$highest_certainty_sofar){
     return(TRUE)
   }else{
-    criterion_args$highest_certainty_sofar <- mean_certainty
+    criterion_args$highest_certainty_sofar <<- mean_certainty
     return(FALSE)
   }
 }
 
 stop_certainty_level <- function(pred){
-  mean_uncertainty = mean(abs(pred - round(pred)))
-  return(abs(1 - mean_uncertainty) < criterion_args$mean_certainty_threshold)
+  mean_certainty = abs(1 - mean(abs(pred - round(pred))))
+  return(mean_certainty > criterion_args$mean_certainty_threshold)
 }
 
 stop_criterion <- function(prev_pred, pred){
-  return(
-    switch(
-      criterion_type,
-      stabilization={stop_stabilization(prev_pred, pred)},
-      certainty_growth={stop_certainty_growth(pred)},
-      certainty_level={stop_certainty_level(pred)}
-    )
-  )
+  if(criterion_types[c_type] == "stabilization"){
+    return(stop_stabilization(prev_pred, pred))
+  }
+
+  if(criterion_types[c_type] == "certainty_growth"){
+    return(stop_certainty_growth(pred))
+  }
+  
+  if(criterion_types[c_type] == "certainty_threshold"){
+    return(stop_certainty_level(pred))
+  }
+  # switch(
+  #   condition,
+  #   stabilization={return(stop_stabilization(prev_pred, pred)) },
+  #   certainty_growth={return(stop_certainty_growth(pred))},
+  #   certainty_level={return(stop_certainty_level(pred))},
+  # )
 }
 
 choose_most_certain <- function(sample_rows, pred){
-  percentile = quantile(pred, 1 - autolabel_percent)
-  most_certain = sample_rows[pred > percentile,]
-  most_certain[, n_cols] = round(pred[pred > percentile])
+  percentile = quantile(abs(pred - round(pred)), autolabel_percent)
+  most_certain = sample_rows[abs(pred - round(pred)) < percentile,]
+  most_certain[, n_cols] = round(pred[abs(pred - round(pred)) < percentile])
   return(most_certain)
 }
 
